35f55b012d46419c8f42806b82093b6845736fb0
Egor Malykh <hello@meownoid.pro>
2024-01-08 18:14:54 +0100

    fix FindRtMidi

--- cmake/FindRtMidi.cmake
+++ cmake/FindRtMidi.cmake
@@ -1,16 +1,14 @@
-
 message (STATUS "Checking for RtMidi...")
-find_path(RTMIDI_INCLUDE_DIRS RtMidi.h
-  PATHS
-    /usr/local/include
-    /usr/include
-    /usr/include/rtmidi
+find_path(
+  RTMIDI_INCLUDE_DIRS RtMidi.h
+  PATH_SUFFIXES rtmidi
   HINTS /tmp/rtmidi-2.1.0 # for travis-ci linux build
 )
 
 if(NOT WIN32)
 
-  find_library(RTMIDI_LIBRARIES
+  find_library(
+    RTMIDI_LIBRARIES
     NAMES
       rtmidi
     PATHS
@@ -21,13 +19,13 @@ if(NOT WIN32)
 
 endif()
 
-if(RTMIDI_LIBRARIES)
+if(RTMIDI_LIBRARIES AND RTMIDI_INCLUDE_DIRS)
   mark_as_advanced(RTMIDI_LIBRARIES RTMIDI_INCLUDE_DIRS)
 
-if(APPLE)
-  set(RTMIDI_LINKER_FLAGS "-framework CoreFoundation" "-framework IOKit" "-framework CoreAudio")
-  set(RTMIDI_LINKER_FLAGS ${RTMIDI_LINKER_FLAGS} "-framework CoreMIDI" objc ${RTMIDI_LIBRARIES})
-endif()
+  if(APPLE)
+    set(RTMIDI_LINKER_FLAGS "-framework CoreFoundation" "-framework IOKit" "-framework CoreAudio")
+    set(RTMIDI_LINKER_FLAGS ${RTMIDI_LINKER_FLAGS} "-framework CoreMidi" objc ${RTMIDI_LIBRARIES})
+  endif()
 
   set(RTMIDI_FOUND true)
   message (STATUS "Found: RtMidi")
